f64b506f92df3ff6734b2c7ee043989a106e2f4e
Nipun Chamikara <theharmlesscreeper2002@gmail.com>
2021-08-02 09:30:13 +0530

    Updated packages Changed tab space width (4 spaces to 2 spaces) Fixed bug where closing two or more notes in close succession can cause one note to disappear without getting deleted When input field loses focus, the content is trimmed to remove leading and trailing whitespace Updated spellings (changed from "todo" to "to do")

--- src/Todo.js
+++ src/Todo.js
@@ -1,44 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Todo.css";
 
-export default function Todo({ text, completed, index, updateTodo, addTodo, deleteTodo}) {
-    const [visible, setVisible] = useState(false);
+export default function Todo({
+  text,
+  completed,
+  index,
+  updateTodo,
+  addTodo,
+  deleteTodo,
+}) {
+  // Whether app will be visible or not
+  const [visible, setVisible] = useState(false);
 
-    // Creates a new todo when user hits the enter key
-    const handleKeyDown = (e) => {
-        if (e.key === "Enter") {
-            addTodo();
-        }
+  // Reference to component
+  const todoRef = useRef();
+
+  // Creates a new todo when user hits the enter key
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
     }
+  };
 
-    // Sets visible to true when the component is mounted
-    useEffect(() => {
+  // Sets visible to true when the component is mounted
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
+  // Sets visible to false when the component is unmounted
+  const handleClick = () => {
+    setVisible(false);
+    setTimeout(() => {
+      deleteTodo(index);
+      // If component hasn't been unmounted, sets visible to true
+      if (todoRef.current) {
         setVisible(true);
-    }, [])
+      }
+    }, 300);
+  };
 
-    // Sets visible to false when the component is unmounted
-    const handleClick = () => {
-        setTimeout(() => {
-            deleteTodo(index);
-        }, 300);
-        setVisible(false);
-    }
-    return (
-        <div className={visible ? "Todo active" : "Todo"}>
-            <input
-                className="checkbox"
-                type="checkbox" 
-                checked={completed}
-                onChange={() => updateTodo(index, text, !completed)}
-            />
-            <input 
-                autoFocus 
-                onChange={(e) => updateTodo(index, e.target.value, completed)} 
-                onKeyDown={handleKeyDown} 
-                className={completed ? "complete" : ""}
-                value={text} 
-            />
-            <span className="exit" onClick={handleClick}>×</span>
-        </div>
-    );
+  return (
+    <div ref={todoRef} className={visible ? "Todo active" : "Todo"}>
+      <input
+        className="checkbox"
+        type="checkbox"
+        checked={completed}
+        onChange={() => updateTodo(index, text, !completed)}
+      />
+      <input
+        autoFocus
+        onChange={(e) => updateTodo(index, e.target.value, completed)}
+        onKeyDown={handleKeyDown}
+        className={completed ? "complete" : ""}
+        value={text}
+        onBlur={(e) => updateTodo(index, e.target.value.trim(), completed)}
+      />
+      <span className="exit" onClick={handleClick}>
+        ×
+      </span>
+    </div>
+  );
 }
